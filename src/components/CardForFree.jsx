5ca3fd95272fbbda31d3075e97a4fc19639a9ce0
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-09-17 15:00:45 -0500

    feat: Page Freja

--- src/components/CardForFree.jsx
+++ src/components/CardForFree.jsx
@@ -2,16 +2,16 @@ import pulserRed from "../assets/pulser-red.jpg";
 
 export default function CardForFree({ title }) {
   return (
-    <a href="#" class="group">
-      <figure class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+    <a href="#" className="group">
+      <figure className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <img
           src={pulserRed.src}
           alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-          class="h-full w-full object-cover object-center group-hover:opacity-75"
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
         />
       </figure>
-      <h3 class="mt-4 text-sm text-gray-700">{title}</h3>
-      <p class="mt-1 text-lg font-medium text-gray-900">Free</p>
+      <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">Free</p>
     </a>
   );
 }
